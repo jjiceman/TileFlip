39137e415951e03e018650febf83e14b8dc7566e
Jamie Palmer <jjiceman2007@gmail.com>
2011-11-16 20:04:30 -0500

    stages++

--- src/com/jamie/tileflip/Pieces.java
+++ src/com/jamie/tileflip/Pieces.java
@@ -1,5 +1,7 @@
 package com.jamie.tileflip;
 
+import java.util.Random;
+
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,28 +12,89 @@ import android.widget.ImageView;
 public class Pieces extends BaseAdapter {
 
 	private Context mContext;
-	private int[] pieces = { 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, };
+	private int stage;
+	private int[][] pieces;
 
-	public Pieces(Context c) {
+	public Pieces(Context c, int stageSize) {
+		stage = stageSize;
+		pieces = new int[stageSize][stageSize];
 		mContext = c;
+
+		Random r = new Random();
+		int shuffle = 5 + r.nextInt(10);
+
+		for (int i = 0; i < shuffle; i++) {
+			int chosenOne = r.nextInt(stage * stage);
+			flipPieceAndAjcacent(chosenOne);
+		}
+	}
+
+	public void flipPieceAndAjcacent(int pieceId) {
+		flipPiece(pieceId);
+		flipAdjacent(pieceId);
 	}
 
 	public void setPiece(int pieceId, int value) {
-		pieces[pieceId] = value;
+		pieces[to2D(pieceId)[0]][to2D(pieceId)[1]] = value;
 	}
 
 	public void flipPiece(int pieceId) {
-		pieces[pieceId] = Math.abs(pieces[pieceId] - 1);
+		pieces[to2D(pieceId)[0]][to2D(pieceId)[1]] = Math.abs(pieces[to2D(pieceId)[0]][to2D(pieceId)[1]] - 1);
+	}
+
+	public void flipPiece(int x, int y) {
+		pieces[y][x] = Math.abs(pieces[y][x] - 1);
+	}
+
+	public void flipAdjacent(int pieceId) {
+		int x = to2D(pieceId)[1];
+		int y = to2D(pieceId)[0];
+
+		if (x > 0)
+			flipPiece(x - 1, y);
+
+		if (x < stage - 1)
+			flipPiece(x + 1, y);
+
+		if (y > 0)
+			flipPiece(x, y - 1);
+
+		if (y < stage - 1)
+			flipPiece(x, y + 1);
+	}
+
+	public boolean checkWin() {
+		int winningPiece = pieces[0][0];
+
+		for (int i = 0; i < pieces.length; i++) {
+			for (int j = 0; j < pieces[0].length; j++) {
+				if (pieces[i][j] != winningPiece)
+					return false;
+			}
+		}
+
+		return true;
+	}
+
+	private int[] to2D(int pieceId) {
+		if (pieceId < stage) {
+			return new int[] { 0, pieceId };
+		}
+		return new int[] { (pieceId - (pieceId % stage)) / stage, pieceId % stage };
+	}
+
+	private int toId(int x, int y) {
+		return x % stage + y;
 	}
 
 	@Override
 	public int getCount() {
-		return pieces.length;
+		return pieces.length * pieces[0].length;
 	}
 
 	@Override
-	public Object getItem(int arg0) {
-		return pieces[arg0];
+	public Object getItem(int pieceId) {
+		return pieces[to2D(pieceId)[0]][to2D(pieceId)[1]];
 	}
 
 	@Override
@@ -50,8 +113,8 @@ public class Pieces extends BaseAdapter {
 		} else {
 			imageView = (ImageView) convertView;
 		}
-		imageView.setImageResource(pieces[position] == 0 ? R.drawable.red : R.drawable.green);
+		imageView.setBackgroundColor(0x80000000);
+		imageView.setImageResource(pieces[to2D(position)[0]][to2D(position)[1]] == 0 ? R.drawable.red : R.drawable.green);
 		return imageView;
 	}
-
 }
--- src/com/jamie/tileflip/TileFlipActivity.java
+++ src/com/jamie/tileflip/TileFlipActivity.java
@@ -1,28 +1,44 @@
 package com.jamie.tileflip;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
+import android.widget.Toast;
 
 public class TileFlipActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        final Pieces p = new Pieces(this);
 
-	    GridView gridview = (GridView) findViewById(R.id.gridView1);
-	    gridview.setAdapter(p);
-	    gridview.setOnItemClickListener(new OnItemClickListener() {
-	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-	        	p.flipPiece(position);
-	        	p.notifyDataSetChanged();
-	        }
-	    });
-    }
+	static Pieces p;
+	static int stage = 6;
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		final Context c = this;
+		p = new Pieces(c, stage);
+
+		final GridView gridview = (GridView) findViewById(R.id.gridView1);
+		gridview.setAdapter(p);
+		gridview.setNumColumns(stage);
+		
+		gridview.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+				p.flipPieceAndAjcacent(position);
+				boolean b = p.checkWin();
+				if (b) {
+					Log.d("Info", "Winner!");
+					stage++;
+					gridview.setNumColumns(stage);
+					p = new Pieces(c, stage);
+				}
+				p.notifyDataSetChanged();
+			}
+		});
+	}
+
 }
